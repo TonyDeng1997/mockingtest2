2e56407eac4d01781ebc5063ee67b5f7231eef0b
xiaofeng li <xlics05@gmail.com>
2018-09-05 23:38:14 -0400

    add annotation

--- src/main/java/com/mocking/auth/repository/RoleDao.java
+++ src/main/java/com/mocking/auth/repository/RoleDao.java
@@ -2,6 +2,7 @@ package com.mocking.auth.repository;
 
 import com.mocking.auth.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+/*@author feifei*/
 
 public interface RoleDao extends JpaRepository<Role, Long>{
 }
--- src/main/java/com/mocking/auth/repository/SubscriptionDao.java
+++ src/main/java/com/mocking/auth/repository/SubscriptionDao.java
@@ -2,10 +2,11 @@ package com.mocking.auth.repository;
 
 
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mocking.auth.model.Subscription;
-
+/*@author feifei*/
 public interface SubscriptionDao extends JpaRepository<Subscription, Long>{
 	
 }
