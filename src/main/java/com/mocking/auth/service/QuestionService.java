ce25b07011107a12302a14c3419a1b9f67744b93
xiaofeng li <xlics05@gmail.com>
2018-09-18 21:58:25 -0400

    code clean up and add new endpoint

--- src/main/java/com/mocking/auth/service/QuestionService.java
+++ src/main/java/com/mocking/auth/service/QuestionService.java
@@ -1,9 +1,9 @@
 package com.mocking.auth.service;
 
-import com.mocking.auth.model.question_data;
+import com.mocking.auth.model.MockingConfig;
+import com.mocking.auth.model.Question_data;
 
 public interface QuestionService {
-    void save(question_data data);
-
-    
+    void save(Question_data data);
+	Question_data findQuestionDataByTitle(String title);
 }
