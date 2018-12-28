9948b760fa0dbfbfae69400bd5b5ec8f245420dd
xiaofeng li <xlics05@gmail.com>
2018-12-27 22:40:24 -0500

    1/ update annotation to springboot 2/ test it ok

--- src/main/java/com/mocking/auth/ws/QuestionController.java
+++ src/main/java/com/mocking/auth/ws/QuestionController.java
@@ -5,11 +5,10 @@ package com.mocking.auth.ws;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import com.mocking.auth.model.QuestionData;
 import com.mocking.auth.service.QuestionService;
 
@@ -23,7 +22,7 @@ public class QuestionController {
 
     //TODO test this and add a jsp file.
     //https://leetcode.com/problems/two-sum/description/
-    @RequestMapping(value = "problem/{title}/description", method = RequestMethod.GET)
+    @GetMapping(value = "problem/{title}/description")
     public String getQuestion(Model model, @PathVariable String title) {
     	QuestionData question = questionService.findQuestionDataByTitle(title);
         model.addAttribute("questionForm", question);
@@ -31,7 +30,7 @@ public class QuestionController {
     }
 
     //TODO, need to create a web form just like userForm
-    @RequestMapping(value = "problems/submit", method = RequestMethod.POST)
+    @PostMapping(value = "problems/submit")
     public String saveMockdingConfig(@ModelAttribute("questionForm") QuestionData questionForm, Model model) {
         // TODO need to implement a mockingConfigValidator
     	questionService.save(questionForm);
--- src/main/java/com/mocking/auth/ws/UserController.java
+++ src/main/java/com/mocking/auth/ws/UserController.java
@@ -8,7 +8,6 @@ import com.mocking.auth.validator.UserValidator;
 import util.RunCode;
 import util.SourceCode;
 import util.ConfigProperties;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -17,92 +16,91 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /*@author feifei*/
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private SecurityService securityService;
+	@Autowired
+	private SecurityService securityService;
 
-    @Autowired
-    private UserValidator userValidator;
+	@Autowired
+	private UserValidator userValidator;
 
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registration(Model model) {
-        model.addAttribute("userForm", new User());
+	@GetMapping(value = "/registration")
+	public String registration(Model model) {
+		model.addAttribute("userForm", new User());
 
-        return "registration";
-    }
+		return "registration";
+	}
 
-    @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
-        userValidator.validate(userForm, bindingResult);
+	@PostMapping(value = "/registration")
+	public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+		userValidator.validate(userForm, bindingResult);
 
-        if (bindingResult.hasErrors()) {
-            return "registration";
-        }
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
 
-        userService.save(userForm);
+		userService.save(userForm);
 
-        securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
+		securityService.autologin(userForm.getUsername(), userForm.getPasswordConfirm());
 
-        return "redirect:/welcome";
-    }
+		return "redirect:/welcome";
+	}
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
+	@GetMapping(value = "/login")
+	public String login(Model model, String error, String logout) {
+		if (error != null)
+			model.addAttribute("error", "Your username and password is invalid.");
 
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
+		if (logout != null)
+			model.addAttribute("message", "You have been logged out successfully.");
 
-        return "login";
-    }
+		return "login";
+	}
 
-    @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
-    public String welcome(Model model) {
-        return "welcome";
-    }
-    
-    @RequestMapping(value = "timer", method = RequestMethod.GET)
-    public String getTimer(Model model) {
-    	model.addAttribute("form", new TestForm());
-        return "timer";
-    }
-    @RequestMapping(value = {"/test"}, method = RequestMethod.POST)
-    public ModelAndView test(@ModelAttribute("form") TestForm form,Model model) throws IOException  {
-    	System.out.println(form.getSource_code());
-    	ConfigProperties ohla=new ConfigProperties();
-    	
-    	
-    
-    	SourceCode UserCode=new SourceCode("Hello",form.getSource_code(),"java");
-    	
-    	RunCode TheProcess=new RunCode(UserCode);
-    	Path hello = TheProcess.executeCode();
-    	Map<String, Object> params = new HashMap<>();
-    	FileReader fr;
+	@GetMapping(value = { "/", "/welcome" })
+	public String welcome(Model model) {
+		return "welcome";
+	}
+
+	@GetMapping(value = "timer")
+	public String getTimer(Model model) {
+		model.addAttribute("form", new TestForm());
+		return "timer";
+	}
+
+	@PostMapping(value = { "/test" })
+	public ModelAndView test(@ModelAttribute("form") TestForm form, Model model) throws IOException {
+		System.out.println(form.getSource_code());
+		ConfigProperties ohla = new ConfigProperties();
+
+		// TODO Fix here, big bug
+		SourceCode UserCode = new SourceCode("Hello", form.getSource_code(), "java");
+
+		RunCode TheProcess = new RunCode(UserCode);
+		Path hello = TheProcess.executeCode();
+		Map<String, Object> params = new HashMap<>();
+		FileReader fr;
 		try {
 			String data = "";
-		    data = new String(Files.readAllBytes(Paths.get(hello+"/"+"log.txt")));
-		    System.out.println(data);
-		    
-		    params.put("feedback", data);
-		    System.out.println("done reading file");
+			data = new String(Files.readAllBytes(Paths.get(hello + "/" + "log.txt")));
+			System.out.println(data);
+
+			params.put("feedback", data);
+			System.out.println("done reading file");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -110,11 +108,9 @@ public class UserController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    		 
-    	
-    	System.out.println("done");
-    	
-    	
-    	return new ModelAndView("timer", params);
-    }
+
+		System.out.println("done");
+
+		return new ModelAndView("timer", params);
+	}
 }
